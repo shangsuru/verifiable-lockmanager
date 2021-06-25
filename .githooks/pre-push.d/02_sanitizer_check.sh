70c6c51687890ce0a7084fddfcb4f693a14e46ef
shangsuru <helm.henry99@gmail.com>
2021-06-25 09:07:49 +0200

    TDBT-137 Restore sanitizer script

--- .githooks/pre-push.d/02_sanitizer_check.sh
+++ .githooks/pre-push.d/02_sanitizer_check.sh
@@ -40,17 +40,16 @@ done
 # Perform address analysis of code, e.g., using clang-tidy
 echo '[git hook] running build with address sanitizier'
 
-BUILDFOLDER=build
+BUILDFOLDER=build-san
 # Set up a compile command database
 # -DCMAKE_BUILD_TYPE=asan will enable AddressSanitizer to detect memory leaks
 # -S refers to the folder with the source files and -B to the build folder
 cmake -S . -B ${BUILDFOLDER} -DCMAKE_CXX_COMPILER=${COMPILER} -DCMAKE_BUILD_TYPE=Debug -DWITH_ASAN=ON -DCMAKE_CXX_STANDARD_LIBRARIES="-lcurl"
 # We have to build the tests
-# cmake --build ${BUILDFOLDER}
+cmake --build ${BUILDFOLDER}
 # Now we can trigger the test to run
-# cmake --build ${BUILDFOLDER} --target test -- ARGS="-R Stub* --output-on-failure"
+cmake --build ${BUILDFOLDER} --target test -- ARGS="-R Stub* --output-on-failure"
 result=$?
 
-# rm -rf ${BUILDFOLDER}
-# exit $resultd
-exit 0;
+rm -rf ${BUILDFOLDER}
+exit $result
