7c009eda2f692596e0a88e30dd36b57b5b1d6302
shangsuru <helm.henry99@gmail.com>
2021-06-28 06:08:00 +0200

    TDBT-137 Document the lock class

--- include/lockmanager/lock.h
+++ include/lockmanager/lock.h
@@ -14,10 +14,38 @@ class Lock {
    * or exclusive, then it allows only one write access
    */
   enum LockMode { kShared, kExclusive };
+
+  /**
+   * @returns the mode (shared, exclusive) the lock is in
+   */
   auto getMode() -> LockMode;
+
+  /**
+   * Attempts to acquire shared access for a transaction.
+   *
+   * @throws std::domain_error, when the lock is exclusive
+   */
   void getSharedAccess();
+
+  /**
+   * Attempts to acquire exclusive access for a transaction.
+   *
+   * @throws std::domain_error, if someone already has that lock
+   */
   void getExclusiveAccess();
+
+  /**
+   * Releases the lock for the calling transaction.
+   */
   void release();
+
+  /**
+   * Upgrades the lock for the transaction, that currently holds the shared lock
+   * alone, to an exclusive lock.
+   *
+   * @throws std::domain_error, if some other transaction currently still has
+   * shared access to the lock
+   */
   void upgrade();
 
  private:
