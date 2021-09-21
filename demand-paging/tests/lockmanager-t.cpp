aaecac83bcf5ef5913f214a715b97fefce3a8f0f
Henry Helm <henryhelm@outlook.com>
2021-09-21 16:02:02 +0000

    TDBT-204 DEMAND-PAGING Fix server and lockmanager tests

--- demand-paging/tests/lockmanager-t.cpp
+++ demand-paging/tests/lockmanager-t.cpp
@@ -12,71 +12,56 @@ const unsigned int kRowId = 0;
 // Lock request aborts, when transaction is not registered
 TEST(LockManagerTest, lockRequestAbortsWhenTransactionNotRegistered) {
   LockManager lock_manager = LockManager();
-  try {
-    lock_manager.lock(kTransactionIdA, kRowId, Lock::LockMode::kShared);
-    FAIL() << "Expected std::domain_error";
-  } catch (const std::domain_error& e) {
-    EXPECT_EQ(e.what(), std::string("Transaction was not registered"));
-  } catch (...) {
-    FAIL() << "Expected std::domain_error";
-  }
+  EXPECT_FALSE(
+      lock_manager.lock(kTransactionIdA, kRowId, Lock::LockMode::kShared)
+          .second);
 };
 
 // Registering an already registered transaction
 TEST(LockManagerTest, cannotRegisterTwice) {
   LockManager lock_manager = LockManager();
-  try {
-    lock_manager.registerTransaction(kTransactionIdA, kLockBudget);
-    lock_manager.registerTransaction(kTransactionIdA, kLockBudget);
-    FAIL() << "Expected std::domain_error";
-  } catch (const std::domain_error& e) {
-    EXPECT_EQ(e.what(), std::string("Transaction already registered"));
-  } catch (...) {
-    FAIL() << "Expected std::domain_error";
-  }
+  EXPECT_TRUE(lock_manager.registerTransaction(kTransactionIdA, kLockBudget));
+  EXPECT_FALSE(lock_manager.registerTransaction(kTransactionIdA, kLockBudget));
 };
 
 // Acquiring non-conflicting shared and exclusive locks works
 TEST(LockManagerTest, acquiringLocks) {
   LockManager lock_manager = LockManager();
-  lock_manager.registerTransaction(kTransactionIdA, kLockBudget);
-  lock_manager.lock(kTransactionIdA, kRowId, Lock::LockMode::kShared);
-  lock_manager.lock(kTransactionIdA, kRowId + 1,
-                                              Lock::LockMode::kExclusive);
+  EXPECT_TRUE(lock_manager.registerTransaction(kTransactionIdA, kLockBudget));
+  EXPECT_TRUE(
+      lock_manager.lock(kTransactionIdA, kRowId, Lock::LockMode::kShared)
+          .second);
+  EXPECT_TRUE(
+      lock_manager.lock(kTransactionIdA, kRowId + 1, Lock::LockMode::kExclusive)
+          .second);
 };
 
 // Cannot get exclusive access when someone already has shared access
 TEST(LockManagerTest, wantExclusiveButAlreadyShared) {
   LockManager lock_manager = LockManager();
-  lock_manager.registerTransaction(kTransactionIdA, kLockBudget);
-  lock_manager.registerTransaction(kTransactionIdB, kLockBudget);
+  EXPECT_TRUE(lock_manager.registerTransaction(kTransactionIdA, kLockBudget));
+  EXPECT_TRUE(lock_manager.registerTransaction(kTransactionIdB, kLockBudget));
 
-  lock_manager.lock(kTransactionIdA, kRowId, Lock::LockMode::kShared);
-  try {
-    lock_manager.lock(kTransactionIdB, kRowId, Lock::LockMode::kExclusive);
-    FAIL() << "Expected std::domain error";
-  } catch (const std::domain_error& e) {
-    EXPECT_EQ(e.what(), std::string("Couldn't acquire lock"));
-  } catch (...) {
-    FAIL() << "Expected std::domain error";
-  }
+  EXPECT_TRUE(
+      lock_manager.lock(kTransactionIdA, kRowId, Lock::LockMode::kShared)
+          .second);
+  EXPECT_FALSE(
+      lock_manager.lock(kTransactionIdB, kRowId, Lock::LockMode::kExclusive)
+          .second);
 };
 
 // Cannot get shared access, when someone has exclusive access
 TEST(LockManagerTest, wantSharedButAlreadyExclusive) {
   LockManager lock_manager = LockManager();
-  lock_manager.registerTransaction(kTransactionIdA, kLockBudget);
-  lock_manager.registerTransaction(kTransactionIdB, kLockBudget);
+  EXPECT_TRUE(lock_manager.registerTransaction(kTransactionIdA, kLockBudget));
+  EXPECT_TRUE(lock_manager.registerTransaction(kTransactionIdB, kLockBudget));
 
-  lock_manager.lock(kTransactionIdA, kRowId, Lock::LockMode::kExclusive);
-  try {
-    lock_manager.lock(kTransactionIdB, kRowId, Lock::LockMode::kShared);
-    FAIL() << "Expected std::domain error";
-  } catch (const std::domain_error& e) {
-    EXPECT_EQ(e.what(), std::string("Couldn't acquire lock"));
-  } catch (...) {
-    FAIL() << "Expected std::domain error";
-  }
+  EXPECT_TRUE(
+      lock_manager.lock(kTransactionIdA, kRowId, Lock::LockMode::kExclusive)
+          .second);
+  EXPECT_FALSE(
+      lock_manager.lock(kTransactionIdB, kRowId, Lock::LockMode::kShared)
+          .second);
 };
 
 // Several transactions can acquire a shared lock on the same row
@@ -84,101 +69,105 @@ TEST(LockManagerTest, multipleTransactionsSharedLock) {
   LockManager lock_manager = LockManager();
   for (unsigned int transaction_id = 0; transaction_id < kLockBudget;
        transaction_id++) {
-    lock_manager.registerTransaction(transaction_id, kLockBudget);
-    lock_manager.lock(transaction_id, kRowId, Lock::LockMode::kShared);
+    EXPECT_TRUE(lock_manager.registerTransaction(transaction_id, kLockBudget));
+    EXPECT_TRUE(
+        lock_manager.lock(transaction_id, kRowId, Lock::LockMode::kShared)
+            .second);
   }
 };
 
 // Cannot get the same lock twice
 TEST(LockManagerTest, sameLockTwice) {
   LockManager lock_manager = LockManager();
-  lock_manager.registerTransaction(kTransactionIdA, kLockBudget);
-  lock_manager.lock(kTransactionIdA, kRowId, Lock::LockMode::kShared);
-  try {
-    lock_manager.lock(kTransactionIdA, kRowId, Lock::LockMode::kShared);
-    FAIL() << "Expected std::domain_error";
-  } catch (const std::domain_error& e) {
-    EXPECT_EQ(e.what(), std::string("Request for already acquired lock"));
-  } catch (...) {
-    FAIL() << "Expected std::domain_error";
-  }
+  EXPECT_TRUE(lock_manager.registerTransaction(kTransactionIdA, kLockBudget));
+  EXPECT_TRUE(
+      lock_manager.lock(kTransactionIdA, kRowId, Lock::LockMode::kShared)
+          .second);
+  EXPECT_FALSE(
+      lock_manager.lock(kTransactionIdA, kRowId, Lock::LockMode::kShared)
+          .second);
 };
 
 // Lock budget runs out
 TEST(LockManagerTest, lockBudgetRunsOut) {
   LockManager lock_manager = LockManager();
-  lock_manager.registerTransaction(kTransactionIdA, kLockBudget);
+  EXPECT_TRUE(lock_manager.registerTransaction(kTransactionIdA, kLockBudget));
 
   unsigned int row_id = 0;
   for (; row_id < kLockBudget; row_id++) {
-    lock_manager.lock(kTransactionIdA, row_id, Lock::LockMode::kShared);
+    EXPECT_TRUE(
+        lock_manager.lock(kTransactionIdA, row_id, Lock::LockMode::kShared)
+            .second);
     std::string expected_signature = "0-" + std::to_string(row_id) + "S-0";
   }
 
-  try {
-    lock_manager.lock(kTransactionIdA, row_id + 1, Lock::LockMode::kShared);
-    FAIL() << "Expected std::domain_error";
-  } catch (const std::domain_error& e) {
-    EXPECT_EQ(e.what(), std::string("Lock budget exhausted"));
-  } catch (...) {
-    FAIL() << "Expected std::domain_error";
-  }
+  EXPECT_FALSE(
+      lock_manager.lock(kTransactionIdA, row_id + 1, Lock::LockMode::kShared)
+          .second);
 };
 
 // Can upgrade a lock
 TEST(LockManagerTest, upgradeLock) {
   LockManager lock_manager = LockManager();
-  lock_manager.registerTransaction(kTransactionIdA, kLockBudget);
-  lock_manager.lock(kTransactionIdA, kRowId, Lock::LockMode::kShared);
-  lock_manager.lock(kTransactionIdA, kRowId, Lock::LockMode::kExclusive);
+  EXPECT_TRUE(lock_manager.registerTransaction(kTransactionIdA, kLockBudget));
+  EXPECT_TRUE(
+      lock_manager.lock(kTransactionIdA, kRowId, Lock::LockMode::kShared)
+          .second);
+  EXPECT_TRUE(
+      lock_manager.lock(kTransactionIdA, kRowId, Lock::LockMode::kExclusive)
+          .second);
 };
 
 // Can unlock and acquire again
 TEST(LockManagerTest, unlock) {
   LockManager lock_manager = LockManager();
-  lock_manager.registerTransaction(kTransactionIdA, kLockBudget);
-  lock_manager.registerTransaction(kTransactionIdB, kLockBudget);
-  lock_manager.registerTransaction(kTransactionIdC, kLockBudget);
+  EXPECT_TRUE(lock_manager.registerTransaction(kTransactionIdA, kLockBudget));
+  EXPECT_TRUE(lock_manager.registerTransaction(kTransactionIdB, kLockBudget));
+  EXPECT_TRUE(lock_manager.registerTransaction(kTransactionIdC, kLockBudget));
 
-  lock_manager.lock(kTransactionIdA, kRowId, Lock::LockMode::kShared);
-  lock_manager.lock(kTransactionIdB, kRowId, Lock::LockMode::kShared);
+  EXPECT_TRUE(
+      lock_manager.lock(kTransactionIdA, kRowId, Lock::LockMode::kShared)
+          .second);
+  EXPECT_TRUE(
+      lock_manager.lock(kTransactionIdB, kRowId, Lock::LockMode::kShared)
+          .second);
 
   lock_manager.unlock(kTransactionIdA, kRowId);
   lock_manager.unlock(kTransactionIdB, kRowId);
 
-  lock_manager.lock(kTransactionIdC, kRowId, Lock::LockMode::kExclusive);
+  EXPECT_TRUE(
+      lock_manager.lock(kTransactionIdC, kRowId, Lock::LockMode::kExclusive)
+          .second);
 };
 
 // Cannot request more locks after transaction aborted
 TEST(LockManagerTest, noMoreLocksAfterAbort) {
   LockManager lock_manager = LockManager();
-  lock_manager.registerTransaction(kTransactionIdA, kLockBudget);
-  lock_manager.lock(kTransactionIdA, kRowId, Lock::LockMode::kShared);
-  lock_manager.lock(kTransactionIdA, kRowId + 1, Lock::LockMode::kShared);
+  EXPECT_TRUE(lock_manager.registerTransaction(kTransactionIdA, kLockBudget));
+  EXPECT_TRUE(
+      lock_manager.lock(kTransactionIdA, kRowId, Lock::LockMode::kShared)
+          .second);
+  EXPECT_TRUE(
+      lock_manager.lock(kTransactionIdA, kRowId + 1, Lock::LockMode::kShared)
+          .second);
 
   // Make transaction abort by acquiring the same lock again
-  try {
-    lock_manager.lock(kTransactionIdA, kRowId + 1, Lock::LockMode::kShared);
-    FAIL() << "Expected std::domain_error";
-  } catch (const std::domain_error& e) {
-    EXPECT_EQ(e.what(), std::string("Request for already acquired lock"));
-  }
+  EXPECT_FALSE(
+      lock_manager.lock(kTransactionIdA, kRowId + 1, Lock::LockMode::kShared)
+          .second);
 
-  try {
-    lock_manager.lock(kTransactionIdA, kRowId + 2, Lock::LockMode::kShared);
-    FAIL() << "Expected std::domain_error";
-  } catch (const std::domain_error& e) {
-    EXPECT_EQ(e.what(), std::string("Transaction was not registered"));
-  } catch (...) {
-    FAIL() << "Expected std::domain_error";
-  }
+  EXPECT_FALSE(
+      lock_manager.lock(kTransactionIdA, kRowId + 2, Lock::LockMode::kShared)
+          .second);
 };
 
 // Releasing a lock twice for the same transaction has no effect
 TEST(LockManagerTest, releaseLockTwice) {
   LockManager lock_manager = LockManager();
-  lock_manager.registerTransaction(kTransactionIdA, kLockBudget);
-  lock_manager.lock(kTransactionIdA, kRowId, Lock::LockMode::kExclusive);
+  EXPECT_TRUE(lock_manager.registerTransaction(kTransactionIdA, kLockBudget));
+  EXPECT_TRUE(
+      lock_manager.lock(kTransactionIdA, kRowId, Lock::LockMode::kExclusive)
+          .second);
   lock_manager.unlock(kTransactionIdA, kRowId);
   lock_manager.unlock(kTransactionIdA, kRowId);
 };
@@ -186,16 +175,12 @@ TEST(LockManagerTest, releaseLockTwice) {
 // Cannot acquire more locks in shrinking phase
 TEST(LockManagerTest, noMoreLocksInShrinkingPhase) {
   LockManager lock_manager = LockManager();
-  lock_manager.registerTransaction(kTransactionIdA, kLockBudget);
-  lock_manager.lock(kTransactionIdA, kRowId, Lock::LockMode::kExclusive);
+  EXPECT_TRUE(lock_manager.registerTransaction(kTransactionIdA, kLockBudget));
+  EXPECT_TRUE(
+      lock_manager.lock(kTransactionIdA, kRowId, Lock::LockMode::kExclusive)
+          .second);
   lock_manager.unlock(kTransactionIdA, kRowId);
-  try {
-    lock_manager.lock(kTransactionIdA, kRowId, Lock::LockMode::kExclusive);
-    FAIL() << "Expected std::domain_error";
-  } catch (const std::domain_error& e) {
-    EXPECT_EQ(e.what(),
-              std::string("Cannot acquire more locks according to 2PL"));
-  } catch (...) {
-    FAIL() << "Expected std::domain_error";
-  }
+  EXPECT_FALSE(
+      lock_manager.lock(kTransactionIdA, kRowId, Lock::LockMode::kExclusive)
+          .second);
 };
