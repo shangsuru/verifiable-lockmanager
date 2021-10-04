95f124a7300476856d130b65ffab545d82603f92
Henry Helm <henryhelm@outlook.com>
2021-10-04 07:49:42 +0000

    TDBT-204 Refactor into lock_signatures.cpp

---
+++ out-of-enclave/include/enclave/lock_signatures.h
@@ -0,0 +1,114 @@
+#pragma once
+
+#include <string>
+
+#include "base64-encoding.h"
+#include "enclave_t.h"
+#include "sgx_tcrypto.h"
+#include "sgx_trts.h"
+#include "sgx_tseal.h"
+
+// Context and public private key pair for signing lock requests
+extern sgx_ecc_state_handle_t context;
+extern sgx_ec256_private_t ec256_private_key;
+extern sgx_ec256_public_t ec256_public_key;
+
+// Struct that gets sealed to storage to persist ECDSA keys
+struct DataToSeal {
+  sgx_ec256_private_t privateKey;
+  sgx_ec256_public_t publicKey;
+};
+
+const size_t MAX_SIGNATURE_LENGTH = 255;
+
+// Base64 encoded public key
+extern std::string encoded_public_key;
+
+/**
+ * Generates keys for ECDSA signature and sets corresponding private and
+ * public key attribute.
+ * @returns SGX_SUCCESS or error code
+ */
+auto generate_key_pair() -> int;
+
+/**
+ * Signs the message and stores the signature in given buffer.
+ * @param message message to sign
+ * @param signature buffer to store the signature
+ * @param sig_len size of signature
+ * @returns SGX_SUCCESS or error code
+ */
+auto sign(const char *message, void *signature, size_t sig_len) -> int;
+
+/**
+ * Verifies given message signature pair.
+ * @param message message to verify
+ * @param signature signature of message
+ * @param sig_len size of signature
+ * @returns SGX_SUCCESS or error code
+ */
+auto verify(const char *message, void *signature, size_t sig_len) -> int;
+
+/**
+ * Closes the ECDSA context.
+ * @returns SGX_SUCCESS or error code
+ */
+auto ecdsa_close() -> int;
+
+/**
+ * This function is just for testing, to demonstrate that signatures created on
+ * lock requests are valid.
+ *
+ * @param signature containing the signature for the lock that was
+ * requested
+ * @param transactionId identifying the transaction that requested the lock
+ * @param rowId identifying the row the lock is refering to
+ * @param isExclusive if the lock is a shared or exclusive lock (boolean)
+ * @returns SGX_SUCCESS, when the signature is valid
+ */
+auto verify_signature(char *signature, int transactionId, int rowId,
+                      int isExclusive) -> int;
+
+/**
+ *  Get string representation of the lock tuple:
+ * <TRANSACTION-ID>_<ROW-ID>_<MODE>_<BLOCKTIMEOUT>, where mode means, if the
+ * lock is for shared or exclusive access.
+ *
+ * @param transactionId identifies the transaction
+ * @param rowId identifies the row that is locked
+ * @param isExclusive if the lock is exclusive or shared
+ * @returns a string that represents a lock, that can be signed by the signing
+ * function
+ */
+auto lock_to_string(int transactionId, int rowId, bool isExclusive)
+    -> std::string;
+
+/**
+ * @returns the block timeout, which resembles a future block number of the
+ *          blockchain in the storage layer. The storage layer will decline
+ *          any requests with a signature that has a block timeout number
+ *          smaller than the current block number
+ */
+auto get_block_timeout() -> int;
+
+/**
+ * @returns the size of the encrypted DataToSeal struct
+ */
+auto get_sealed_data_size() -> uint32_t;
+
+/**
+ * Seals the public and private key and stores it inside the sealed blob.
+ * @param sealed_blob buffer to store sealed keys
+ * @param sealed_size size of the sealed blob buffer
+ * @returns SGX_SUCCESS or error code
+ */
+auto seal_keys(uint8_t *sealed_blob, uint32_t sealed_size) -> sgx_status_t;
+
+/**
+ * Unseals the keys stored in sealed_blob and sets global private and
+ * public key attribute.
+ * @param sealed_blob buffer that contains the sealed public and private keys
+ * @param sealed_size size of the sealed blob buffer
+ * @returns SGX_SUCCESS or error code
+ */
+auto unseal_keys(const uint8_t *sealed_blob, size_t data_size) -> sgx_status_t;
--- out-of-enclave/src/enclave/enclave.cpp
+++ out-of-enclave/src/enclave/enclave.cpp
@@ -231,98 +231,6 @@ void enclave_process_request() {
   return;
 }
 
-auto get_block_timeout() -> int {
-  print_warn("Lock timeout not yet implemented");
-  // TODO: Implement getting the lock timeout
-  return 0;
-};
-
-auto get_sealed_data_size() -> uint32_t {
-  return sgx_calc_sealed_data_size((uint32_t)encoded_public_key.length(),
-                                   sizeof(DataToSeal{}));
-}
-
-auto seal_keys(uint8_t *sealed_blob, uint32_t sealed_size) -> sgx_status_t {
-  print_info("Sealing keys");
-  sgx_status_t ret = SGX_ERROR_INVALID_PARAMETER;
-  sgx_sealed_data_t *sealed_data = NULL;
-  DataToSeal data;
-  data.privateKey = ec256_private_key;
-  data.publicKey = ec256_public_key;
-
-  if (sealed_size != 0) {
-    sealed_data = (sgx_sealed_data_t *)malloc(sealed_size);
-    ret = sgx_seal_data((uint32_t)encoded_public_key.length(),
-                        (uint8_t *)encoded_public_key.c_str(), sizeof(data),
-                        (uint8_t *)&data, sealed_size, sealed_data);
-    if (ret == SGX_SUCCESS)
-      memcpy(sealed_blob, sealed_data, sealed_size);
-    else
-      free(sealed_data);
-  }
-  return ret;
-}
-
-auto unseal_keys(const uint8_t *sealed_blob, size_t sealed_size)
-    -> sgx_status_t {
-  print_info("Unsealing keys");
-  sgx_status_t ret = SGX_ERROR_INVALID_PARAMETER;
-  DataToSeal *unsealed_data = NULL;
-
-  uint32_t dec_size = sgx_get_encrypt_txt_len((sgx_sealed_data_t *)sealed_blob);
-  uint32_t mac_text_len =
-      sgx_get_add_mac_txt_len((sgx_sealed_data_t *)sealed_blob);
-
-  uint8_t *mac_text = (uint8_t *)malloc(mac_text_len);
-  if (dec_size != 0) {
-    unsealed_data = (DataToSeal *)malloc(dec_size);
-    sgx_sealed_data_t *tmp = (sgx_sealed_data_t *)malloc(sealed_size);
-    memcpy(tmp, sealed_blob, sealed_size);
-    ret = sgx_unseal_data(tmp, mac_text, &mac_text_len,
-                          (uint8_t *)unsealed_data, &dec_size);
-    if (ret != SGX_SUCCESS) goto error;
-    ec256_private_key = unsealed_data->privateKey;
-    ec256_public_key = unsealed_data->publicKey;
-  }
-
-error:
-  if (unsealed_data != NULL) free(unsealed_data);
-  return ret;
-}
-
-auto generate_key_pair() -> int {
-  print_info("Creating new key pair");
-  if (context == NULL) sgx_ecc256_open_context(&context);
-  sgx_status_t ret = sgx_ecc256_create_key_pair(&ec256_private_key,
-                                                &ec256_public_key, context);
-  encoded_public_key = base64_encode((unsigned char *)&ec256_public_key,
-                                     sizeof(ec256_public_key));
-
-  // Append the number of characters for the encoded public key for easy
-  // extraction from sealed text file
-  encoded_public_key += std::to_string(encoded_public_key.length());
-  return ret;
-}
-
-// Verifies a given message with its signature object and returns on success
-// SGX_EC_VALID or on failure SGX_EC_INVALID_SIGNATURE
-auto verify(const char *message, void *signature, size_t sig_len) -> int {
-  sgx_ecc_state_handle_t context = NULL;
-  sgx_ecc256_open_context(&context);
-  uint8_t res;
-  sgx_ec256_signature_t *sig = (sgx_ec256_signature_t *)signature;
-  sgx_status_t ret = sgx_ecdsa_verify((uint8_t *)message,
-                                      strnlen(message, MAX_SIGNATURE_LENGTH),
-                                      &ec256_public_key, sig, &res, context);
-  return res;
-}
-
-// Closes the ECDSA context
-auto ecdsa_close() -> int {
-  if (context == NULL) sgx_ecc256_open_context(&context);
-  return sgx_ecc256_close_context(context);
-}
-
 auto acquire_lock(void *signature, int transactionId, int rowId,
                   bool isExclusive) -> bool {
   bool ok;
@@ -456,12 +364,11 @@ sign:
   std::string string_to_sign =
       lock_to_string(transactionId, rowId, lockTrusted->exclusive);
 
-  sgx_ecc_state_handle_t context = NULL;
-  sgx_ecc256_open_context(&context);
+  sgx_ecc_state_handle_t ctx = NULL;
+  sgx_ecc256_open_context(&ctx);
   sgx_ecdsa_sign((uint8_t *)string_to_sign.c_str(),
                  strnlen(string_to_sign.c_str(), MAX_SIGNATURE_LENGTH),
-                 &ec256_private_key, (sgx_ec256_signature_t *)signature,
-                 context);
+                 &ec256_private_key, (sgx_ec256_signature_t *)signature, ctx);
 
   free_lock_bucket_copy(lockEntry);
   free_transaction_bucket_copy(transactionEntry);
@@ -505,41 +412,4 @@ void release_lock(int transactionId, int rowId) {
 
   free_lock_bucket_copy(lockEntry);
   free_transaction_bucket_copy(transactionEntry);
-}
-
-auto verify_signature(char *signature, int transactionId, int rowId,
-                      int isExclusive) -> int {
-  std::string plain = lock_to_string(transactionId, rowId, isExclusive);
-
-  std::string signature_string(signature);
-  std::string x = signature_string.substr(0, signature_string.find("-"));
-  std::string y = signature_string.substr(signature_string.find("-") + 1,
-                                          signature_string.length());
-  sgx_ec256_signature_t sig_struct;
-  memcpy(sig_struct.x, base64_decode(x).c_str(), 8 * sizeof(uint32_t));
-  memcpy(sig_struct.y, base64_decode(y).c_str(), 8 * sizeof(uint32_t));
-
-  int ret =
-      verify(plain.c_str(), (void *)&sig_struct, sizeof(sgx_ec256_signature_t));
-  if (ret != SGX_SUCCESS) {
-    print_error("Failed to verify signature");
-  } else {
-    print_info("Signature successfully verified");
-  }
-  return ret;
-}
-
-auto lock_to_string(int transactionId, int rowId, bool isExclusive)
-    -> std::string {
-  unsigned int block_timeout = get_block_timeout();
-
-  std::string mode;
-  if (isExclusive) {
-    mode = "X";
-  } else {
-    mode = "S";
-  }
-
-  return std::to_string(transactionId) + "_" + std::to_string(rowId) + "_" +
-         mode + "_" + std::to_string(block_timeout);
 }
